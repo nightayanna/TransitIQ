9159f65e27d3aadf2976ec46e162c8d8830ee28e
Brittz <gentlesbrittania1@gmail.com>
2026-03-14 21:39:25 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { AppProvider } from './src/context/AppContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <AppProvider>
+      <AppNavigator />
+    </AppProvider>
+  );
+}
